f5588ce4434754d1699ff514febf0d8364897f9a
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-08-13 19:52:11 +0000

    Add polars to load and manaipulate the data
    - Add an input that takes a country and filters the data by the country
    - Add an action button that gets the data when clicked
    - Add a reactive calculator filters the data with user input and return the result

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 from shiny import ui, render, App, reactive
 from tailwindcss import tailwind
+import polars as pl
+
+df = pl.scan_csv("organizations-2000000.csv")
 
 app_ui = ui.page_fluid(
- ui.div(
-    tailwind,
-    ui.h2("Learning Shiny", class_="text-4xl my-2"),
-    ui.input_numeric(id="num", label="", value=4),
-    ui.input_action_button("compute", "Compute Square"),
- ),
-ui.div(
-    ui.output_text_verbatim("result"),
-    class_="mt-4 font-bold"
-),
-class_="container mx-auto px-4 py-4"
+    ui.div(
+        ui.h2("Learning Shiny", class_="text-4xl my-2"),
+        ui.div(
+            ui.input_text("country", "Search country", width="40%"),
+            ui.input_action_button("search", "Search"),
+            class_="grid grid-col-1",
+        ),
+    ),
+    ui.div(ui.output_table("tablex"), class_="mt-4 font-bold"),
+    class_="container mx-auto px-4 py-4",
 )
 
+
 def server(input, output, session):
+    @reactive.Calc
+    def get_country():
+        if input.country():
+            c = df.filter(pl.col("Country") == f"{input.country().capitalize()}").limit(1000000).collect()
+            return c
+
     @output
-    @render.text
-    @reactive.event(input.compute)
-    def result():
-        return f"{input.num()} squared is: {input.num() ** 2}"
-    
+    @render.table
+    @reactive.event(input.search)
+    def tablex():
+        return get_country()
+
 
 app = App(app_ui, server)
