f95cf443e9a15ca126a462ff1837ac017b77e1bb
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-08-13 16:49:11 +0000

    Control reactivity with a button
    - Add a button input to app
    - Add a reactive input decorator to server function(`@reactive.event(input.compute)`)

--- app.py
+++ app.py
@@ -4,20 +4,16 @@ from shiny import ui, render, App, reactive
 app_ui = ui.div(
  ui.h2("Learning Shiny", class_="text-primary"),
  ui.input_numeric(id="num", label="Number of  Children", value=4),
+ ui.input_action_button("compute", "Compute Square"),
  ui.output_text_verbatim("result")
 )
 
 def server(input, output, session):
-
-    @reactive.Calc
-    def square():
-        return input.num() ** 2
-        
-
     @output
     @render.text
+    @reactive.event(input.compute)
     def result():
-        return f"{input.num()} squared is: {square()}"
+        return f"{input.num()} squared is: {input.num() ** 2}"
     
 
 app = App(app_ui, server)
